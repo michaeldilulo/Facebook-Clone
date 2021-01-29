c16333de052592dc2465cc07896b1349697a47f2
michael dilulo <snoleopard01001101@gmail.com>
2021-01-29 10:23:39 -0800

    Avatar logo and Name

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ import FlagIcon from "@material-ui/icons/Flag"
 import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined"
 import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined"
 import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle"
+import {Avatar} from "@material-ui/core"
 
 function Header() {
     return (
@@ -37,7 +38,13 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header__right"></div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>mdilulo</h4>
+                </div>
+
+            </div>
         </div>
     )
 }
