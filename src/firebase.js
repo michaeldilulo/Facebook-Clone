2448d9998b46021620c43c46d7e6ad296ebf1553
michael dilulo <snoleopard01001101@gmail.com>
2021-01-29 09:57:34 -0800

    material ui / Header and setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDCllBfJ1J7lK2-cJDOnZqxUs_mqHTMizY",
+  authDomain: "facebook-clone-322ae.firebaseapp.com",
+  projectId: "facebook-clone-322ae",
+  storageBucket: "facebook-clone-322ae.appspot.com",
+  messagingSenderId: "752164561882",
+  appId: "1:752164561882:web:6b5e95ee75c767b72916e3"
+};
