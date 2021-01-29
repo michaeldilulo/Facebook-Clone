27ae2d5485ed4bf8cbe896f394eb72cd622d39f0
michael dilulo <snoleopard01001101@gmail.com>
2021-01-29 10:18:10 -0800

    Icons - Middle of the Header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Header.css"
 import SearchIcon from "@material-ui/icons/Search"
+import HomeIcon from "@material-ui/icons/Home"
+import FlagIcon from "@material-ui/icons/Flag"
+import SubscriptionOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined"
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined"
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle"
 
 function Header() {
     return (
@@ -14,7 +19,23 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header__middle"></div>
+            <div className="header__middle">
+                <div className="header__option">
+                <HomeIcon />
+                </div>
+                                <div className="header__option">
+                <FlagIcon />
+                </div>
+                                <div className="header__option">
+                <SubscriptionOutlinedIcon />
+                </div>
+                                <div className="header__option">
+                <StorefrontOutlinedIcon />
+                </div>
+                                <div className="header__option">
+                <SupervisedUserCircleIcon />
+                </div>
+            </div>
 
             <div className="header__right"></div>
         </div>
