9dd3f0af09f8bdff2f0ef53040a4a1507c56120c
michael dilulo <snoleopard01001101@gmail.com>
2021-01-31 09:01:18 -0800

    Prop setup for Sidebar Row Content

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event'
 import React from 'react'
 import "./Sidebar.css"
 import SidebarRow from "./SidebarRow"
@@ -5,7 +6,14 @@ import SidebarRow from "./SidebarRow"
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow />
+            <SidebarRow src={user.photoURL} title={user.displayName} />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+            <SidebarRow Icon={EmojiFlagIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />    
+            <SidebarRow Icon={ChatIcon} title="Messenger" />    
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
         </div>
     )
 }
--- src/SidebarRow.js
+++ src/SidebarRow.js
@@ -1,9 +1,14 @@
+import {Avatar } from "@material-ui/core"
 import React from 'react'
 import "./SidebarRow.css"
 
-function SidebarRow() {
+function SidebarRow({src, Icon, title}) {
     return (
         <div className="sidebarRow">
+            {src && <Avatar src={src} />}
+            {Icon && <Icon />}
+
+            <h4>{title}</h4>
         </div>
     )
 }
