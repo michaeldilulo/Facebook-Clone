3a77fe2680ee6ca44ccd017d2d735f290c98b22d
michael dilulo <snoleopard01001101@gmail.com>
2021-01-31 08:46:07 -0800

    Sidebar / Sidebar Row

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Sidebar.css"
+import SidebarRow from "./SidebarRow"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+            <SidebarRow />
         </div>
     )
 }
