6077f5c33aa57b17430dc08be07e2d58232b0dad
michael dilulo <snoleopard01001101@gmail.com>
2021-01-31 09:05:24 -0800

    Import from @material-ui/icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,11 +2,18 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 import "./Sidebar.css"
 import SidebarRow from "./SidebarRow"
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital"
+import EmojiFlagIcon from "@material-ui/icons/EmojiFlags"
+import PeopleIcon from "@material-ui/icons/People"
+import ChatIcon from "@material-ui/icons/Chat"
+import StorefrontIcon from "@material-ui/icons/Storefront"
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary"
+import {ExpandMoreOutlined} from "@material-ui/icons"
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow src={user.photoURL} title={user.displayName} />
+            <SidebarRow title='mdilulo' />
             <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
             <SidebarRow Icon={EmojiFlagIcon} title="Pages" />
             <SidebarRow Icon={PeopleIcon} title="Friends" />    
