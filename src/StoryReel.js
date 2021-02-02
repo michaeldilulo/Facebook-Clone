742e15dcfe99e15730212a6f805cc4b44a4b79ab
michael dilulo <snoleopard01001101@gmail.com>
2021-02-01 16:11:21 -0800

    Dummy props for application Story

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './StoryReel.css'
+import Story from './Story'
+import {Avatar} from "@material-ui/core"
 
 function StoryReel() {
     return (
         <div className="storyReel">
-            {/* Story One Component */}
+            <Story image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
+                profileSrc="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e6051b748fc2d4157a842e57e427ad44&v=4"
+                title="Sonny Sangha"
+            />
             {/* Story Two Component */}
             {/* Story Three Component */}
             {/* Story Four Component */}
