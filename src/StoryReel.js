e3d6bca3ebf351f356a43b6cd991a7811c36b56a
michael dilulo <snoleopard01001101@gmail.com>
2021-02-02 16:38:57 -0800

    Style for Feed / Sidebar and App

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -10,7 +10,10 @@ function StoryReel() {
                 profileSrc="https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-puppy-weight.jpg?itok=IwMOwGSX"
                 title="Puppy McGee"
             />
-            {/* Story Two Component */}
+                        <Story image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTBHC2s4NFdzXEsVzvBPGjkrSePQa-8XFuNtQ&usqp=CAU"
+                profileSrc="https://www.petmd.com/sites/default/files/styles/article_image/public/petmd-puppy-weight.jpg?itok=IwMOwGSX"
+                title="Puppy McGee"
+            />
             {/* Story Three Component */}
             {/* Story Four Component */}
             {/* Story Five Component */}
