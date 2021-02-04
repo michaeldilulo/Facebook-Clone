a7467798c42764761ff933d18ec70b400c0fde7e
michael dilulo <snoleopard01001101@gmail.com>
2021-02-03 16:44:44 -0800

    imageUrl, value and onChange

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -7,7 +7,7 @@ import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
 
 function MessageSender() {
     const [input, setInput] = useState('');
-
+    const [imageUrl, setImageUrl] = useState('')
 
 
     const handleSubmit = (e) => {
@@ -20,7 +20,7 @@ function MessageSender() {
                 <Avatar />
                 <form>
                     <input value={input} onChange={(e) => setInput(e.target.value)} type="text" className="messageSender__input" placeholder={`What's on your mind?`} />
-                    <input placeholder="image URL (Optional)" />
+                    <input value={input} onChange={(e) => setImageUrl(e.target.value)} placeholder="image URL (Optional)" />
                     <button onClick={handleSubmit} type="submit">
                     Hidden submit
                     </button>
