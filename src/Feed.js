16dc1086d5ff19acee8db63e084ad94d3bfb98fa
michael dilulo <snoleopard01001101@gmail.com>
2021-02-05 17:41:55 -0800

    Style for Post CSS

--- src/Feed.js
+++ src/Feed.js
@@ -17,6 +17,13 @@ function Feed() {
                 username="Puppy"
                 image="https://thehappypuppysite.com/wp-content/uploads/2015/09/The-Siberian-Husky-HP-long.jpg"
             />
+
+            <Post 
+                profilePic="https://cdn11.bigcommerce.com/s-oe2q4reh/images/stencil/2048x2048/products/738/1295/Siberian_Husky_Puppy__60707.1568880761.jpg?c=2"
+                message="This is wooftastic!!!"
+                timestamp="2/5/2021 6:30 PM"
+                username="Puppy"
+                />
             
         </div>
     )
