3f0c988f2a0f22380cda7feb431da42259d1fbc8
michael dilulo <snoleopard01001101@gmail.com>
2021-02-05 17:34:45 -0800

    Hardcode data to show the post in feed

--- src/Feed.js
+++ src/Feed.js
@@ -10,7 +10,13 @@ function Feed() {
 
             <StoryReel />
             <MessageSender />
-            <Post />
+            <Post
+                profilePic="https://cdn11.bigcommerce.com/s-oe2q4reh/images/stencil/2048x2048/products/738/1295/Siberian_Husky_Puppy__60707.1568880761.jpg?c=2"
+                message="Wow This works!!!"
+                timestamp="2/5/2021 6:30 PM"
+                username="Puppy"
+                image="https://thehappypuppysite.com/wp-content/uploads/2015/09/The-Siberian-Husky-HP-long.jpg"
+            />
             
         </div>
     )
